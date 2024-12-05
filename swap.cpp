a7d2a654fc3eb99b4b527f31585e48803305ddf0
Peer Abdulllah <chaudharyabdullah254@gmail.com>
2024-12-05 14:16:29 +0500

    New code ade

--- swap.cpp
+++ swap.cpp
@@ -7,8 +7,9 @@ int main()
     int a,b;
     cout<<"Enter Numbers for Swap :";
     cin>>a>>b;
+    cout<<"ghp_NEhICGzdDVE0PtZmFG1hwtMUSxzbat4Ner2";
     
-    cout <<"Values before swap are :"<<endl<<"  a = "<<a<<"  b = "<<b<<" "<<endl;
+    cout <<"Values Before swap are :"<<endl<<"  a = "<<a<<"  b = "<<b<<" "<<endl;
     int temp = a ;
     a = b;
     b=temp;
